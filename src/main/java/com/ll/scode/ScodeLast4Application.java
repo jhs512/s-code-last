e8016a52b4420ff6131e4823c483241aa6d71eb0
jhs512 <jangka512@gmail.com>
2023-12-03 22:52:06 +0900

    work

---
+++ src/main/java/com/ll/scode/ScodeLast4Application.java
@@ -0,0 +1,13 @@
+package com.ll.scode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScodeLast4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScodeLast4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/scode/ScodeLast4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.scode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScodeLast4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
